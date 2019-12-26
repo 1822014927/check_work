cf8ec781d3e324e32e09520e1413c12632c60b9e
1822014927@qq.com <1822014927@qq.com>
2019-12-26 16:21:49 +0800

    first

---
+++ app/Http/Controllers/admin/CheckController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CheckController extends Controller
+{
+    //查询用户打卡信息
+    public function selectCheck(){
+
+    }
+    //打卡信息设置
+    public function addCheck(){
+
+    }
+}
